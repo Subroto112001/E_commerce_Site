2957afbb6f4cb4820004dae173c39aa1de6b1799
Subroto112001 <subrotokumarbarman@gmail.com>
2025-07-01 03:35:12 +0600

    update

--- src/Pages/CheckOut.jsx
+++ src/Pages/CheckOut.jsx
@@ -71,6 +71,53 @@ const CheckOut = () => {
             />
           </div>
           {/* Apartment address */}
+          {/* Town/City address */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="companyname"
+              className="font-normal font-poppins text-[16px] text-text1_color"
+            >
+              Town/City<span className="text-red-400">*</span>
+            </label>
+            <input
+              type="text"
+              className="rounded bg-secondary_color px-2 py-5 text-[14px] font-poppins"
+            />
+          </div>
+          {/* Town/City address */}
+          {/*Phone Number  */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="companyname"
+              className="font-normal font-poppins text-[16px] text-text1_color"
+            >
+              Phone Number<span className="text-red-400">*</span>
+            </label>
+            <input
+              type="text"
+              className="rounded bg-secondary_color px-2 py-5 text-[14px] font-poppins"
+            />
+          </div>
+          {/* Phone Number */}
+          {/*Email Address  */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="companyname"
+              className="font-normal font-poppins text-[16px] text-text1_color"
+            >
+              Email Address<span className="text-red-400">*</span>
+            </label>
+            <input
+              type="email"
+              className="rounded bg-secondary_color px-2 py-5 text-[14px] font-poppins"
+            />
+          </div>
+          {/* Email Address */}
+
+          <div className="flex flex-row items-center gap-4">
+            <input type="checkbox" className="w-[24px] h-[24px] text-Secondary2_color "/>
+            <h3 className="font-poppins font-normal text-[16px] text-black">Save this information for faster check-out next time</h3>{" "}
+          </div>
         </div>
       </div>
       {/* billing part */}
